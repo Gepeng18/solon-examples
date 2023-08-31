99a27100fb7f0283cb7a9bdd37e91e759e140373
Gepeng18 <robin_ge@foxmail.com>
2023-08-31 23:17:37 +0800

    add comment

---
+++ 3.Solon-Web/demo3001-helloworld/src/main/java/demo3001/controller/AServiceAAAAAAAA.java
@@ -0,0 +1,7 @@
+package demo3001.controller;
+
+import org.noear.solon.annotation.Component;
+
+@Component
+public class AServiceAAAAAAAA {
+}
---
+++ 3.Solon-Web/demo3001-helloworld/src/main/java/demo3001/controller/HelloService.java
@@ -0,0 +1,10 @@
+package demo3001.controller;
+
+import org.noear.solon.annotation.Component;
+
+@Component
+public class HelloService {
+    public void hello() {
+
+    }
+}
