fd79678a637445036e29dabbc064a7f19592fa65
Gepeng18 <robin_ge@foxmail.com>
2023-09-05 23:17:08 +0800

    add test code

--- 3.Solon-Web/demo3011-web/src/main/java/demo3011/WebApp.java
+++ 3.Solon-Web/demo3011-web/src/main/java/demo3011/WebApp.java
@@ -1,11 +1,23 @@
 package demo3011;
 
 import org.noear.solon.Solon;
+import org.noear.solon.SolonApp;
+import org.noear.solon.core.handle.MethodType;
 
 public class WebApp {
     public static void main(String[] args) {
-        Solon.start(WebApp.class, args, app -> {
+        SolonApp solonApp = Solon.start(WebApp.class, args, app -> {
             app.onError(e -> e.printStackTrace());
+        });
+        //开始之前把上下文置为已泻染
+        solonApp.before("/cache/**", MethodType.HTTP, c -> {
+            c.setRendered(true);
+            System.out.println(1);
+        });
+
+        solonApp.after("/cache/**", MethodType.HTTP, c -> {
+            System.out.println(1);
+            //可对 c.result 进行处理 //并输出
         });
     }
 }
